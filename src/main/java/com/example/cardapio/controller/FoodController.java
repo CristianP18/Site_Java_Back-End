3e0101c6b909ef58025a3afed6d4752ea7fe2252
Cristian Pegoraro <cristian.meusimulador@gmail.com>
2023-09-07 12:31:34 -0500

    verssao 1.0

---
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -0,0 +1,31 @@
+package com.example.cardapio.controller;
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository; // Corrigir nome da classe FoodRepository e importação da List
+import com.example.cardapio.food.FoodReponseDTO;
+import com.example.cardapio.food.FoodRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List; // Corrigir importação da List
+
+@RestController
+@RequestMapping("/food")
+public class FoodController {
+    @Autowired
+    private FoodRepository repository;
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food fooData = new Food(data);
+        repository.save(fooData);
+        return;
+
+    }
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodReponseDTO> getAll() {
+        List<FoodReponseDTO> foodList = repository.findAll().stream().map(FoodReponseDTO::new).toList();
+        return foodList;
+    }
+}
---
+++ src/main/java/com/example/cardapio/food/FoodReponseDTO.java
@@ -0,0 +1,9 @@
+package com.example.cardapio.food;
+
+public record FoodReponseDTO(Long id, String title, String image, Integer price) {
+
+    public FoodReponseDTO(Food food) {
+        this(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
+    }
+
+}
